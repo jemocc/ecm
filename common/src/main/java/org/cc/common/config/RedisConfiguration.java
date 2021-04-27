361fa1c134ca6697a6aca80c45916a9eb7b6f059
cc <cc@mail.com>
2021-04-28 01:11:03 +0800

    add redisson

--- common/src/main/java/org/cc/common/config/RedisConfiguration.java
+++ common/src/main/java/org/cc/common/config/RedisConfiguration.java
@@ -1,5 +1,13 @@
 package org.cc.common.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.cc.common.utils.JsonUtil;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -7,8 +15,12 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+
 @Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+@Setter
 public class RedisConfiguration {
+    private Config redisson;
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory redisConnectionFactory){
@@ -20,4 +32,12 @@ public class RedisConfiguration {
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
+
+    @Bean
+    public RedissonClient redisson() {
+        if (redisson == null)
+            return null;
+        return Redisson.create(redisson);
+    }
+
 }
